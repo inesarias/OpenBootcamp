de046fbdc505837f1f2334ff288c86ddfbb58c34
Inés Arias <114267804+inesarias@users.noreply.github.com>
2022-12-27 19:11:22 -0300

    Delete mi-primer-proyecto-de-node directory

--- mi-primer-proyecto-de-node/index.js
+++
@@ -1,6 +0,0 @@
-//Este es el mensaje de Bienvenida
-console.log("Esta es la puerta de entrada al proyecto");
-
-/*Este es el comentario del medio
-Este es el mensaje que se va a usar para despedir*/
-console.log("Chao Papilo")
