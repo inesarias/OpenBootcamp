b20bccdad9e01dce9b115802e39cba3b77c304fc
Inés Arias <114267804+inesarias@users.noreply.github.com>
2022-12-27 19:12:05 -0300

    Add files via upload

---
+++ Curso-JavaScript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,6 @@
+//Este es el mensaje de Bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*Este es el comentario del medio
+Este es el mensaje que se va a usar para despedir*/
+console.log("Chao Papilo")
