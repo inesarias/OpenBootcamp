648cc6e81f488b2fb99784115691e3326e17c843
Inés Arias <114267804+inesarias@users.noreply.github.com>
2022-12-30 13:29:44 -0300

    Add files via upload

---
+++ Curso-JavaScript/mi-primer-proyecto-de-node/2-Sintaxis,Variables,Palabras/ejercicio-1.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Inés Arias",
+    24,
+    true,
+    new Date(1998, 2,25),
+    { 
+        titulo: "El Principito",
+        autor: "Antoine de Saint-Exupéry",
+        fecha: new Date(1943, 3, 6),
+        url: "https://www.amazon.com/-/es/Antoine-Saint-Exup%C3%A9ry/dp/6070730534/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2Y11025GVXAOF&keywords=el+principito&qid=1672417618&sprefix=el+principit%2Caps%2C286&sr=8-1"
+    },
+];
+
+console.log(lista)
